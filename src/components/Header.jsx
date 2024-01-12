b380e224703d1fedb0ea7c47773af287f692d6e0
Maria <pashkovichma@gmail.com>
2024-01-12 16:59:28 +0100

    Outsourcing Context & State Into a Separate Provider Component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,41 +1,32 @@
-import { useRef, useContext } from 'react';
+import { forwardRef, useImperativeHandle, useRef } from 'react';
+import { createPortal } from 'react-dom';
 
-import CartModal from './CartModal.jsx';
-import { CartContext } from '../store/shopping-cart-context.jsx';
+import Cart from './Cart.jsx';
 
-export default function Header() {
-  const modal = useRef();
-  const { items } = useContext(CartContext);
+const CartModal = forwardRef(function Modal(
+  { title, actions },
+  ref
+) {
+  const dialog = useRef();
 
-  const cartQuantity = items.length;
+  useImperativeHandle(ref, () => {
+    return {
+      open: () => {
+        dialog.current.showModal();
+      },
+    };
+  });
 
-  function handleOpenCartClick() {
-    modal.current.open();
-  }
-
-  let modalActions = <button>Close</button>;
-
-  if (cartQuantity > 0) {
-    modalActions = (
-      <>
-        <button>Close</button>
-        <button>Checkout</button>
-      </>
-    );
-  }
-
-  return (
-    <>
-      <CartModal ref={modal} title="Your Cart" actions={modalActions} />
-      <header id="main-header">
-        <div id="main-title">
-          <img src="logo.png" alt="Elegant model" />
-          <h1>Elegant Context</h1>
-        </div>
-        <p>
-          <button onClick={handleOpenCartClick}>Cart ({cartQuantity})</button>
-        </p>
-      </header>
-    </>
+  return createPortal(
+    <dialog id="modal" ref={dialog}>
+      <h2>{title}</h2>
+      <Cart />
+      <form method="dialog" id="modal-actions">
+        {actions}
+      </form>
+    </dialog>,
+    document.getElementById('modal')
   );
-}
+});
+
+export default CartModal;
